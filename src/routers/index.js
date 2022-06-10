5ea2305ba2b9cf51ee9c794afd45aac17d6c4430
Khai <Quangkhai0901@gmail.com>
2022-06-10 15:49:07 +0700

    push quan li phim

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,13 @@
 "use strict";
 const express = require("express");
+const movieRouter = require("./movies");
 const ticketRouter = require("./tickets");
 const userRouter = require("./users");
 const rootRouter = express.Router();
 
 rootRouter.use("/QuanLyNguoiDung", userRouter);
 rootRouter.use("/QuanLyDatVe", ticketRouter);
+rootRouter.use("/QuanLyPhim", movieRouter);
+
 
 module.exports = rootRouter;
